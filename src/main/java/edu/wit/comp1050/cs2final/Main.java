e6ce55c931ec1f381e946f8cc2a8cfc0ec5ed787
yogipatel <yogipatel@localhost>
2021-11-30 17:09:49 -0500

    Correct Button Layout

--- src/main/java/edu/wit/comp1050/cs2final/Main.java
+++ src/main/java/edu/wit/comp1050/cs2final/Main.java
@@ -1,10 +1,12 @@
 package edu.wit.comp1050.cs2final;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -24,12 +26,19 @@ public class Main extends Application {
         stage.setTitle("Checkers");
 
         /* initialize ui components */
+       
+        
         GridPane boardPane = new GridPane();
+        boardPane.setPadding(new Insets(10));
+        boardPane.setHgap(10);
+        boardPane.setVgap(10);
         
         for ( int r = 0; r < 8;r ++) { 
         	for ( int c = 0; c < 8; c ++ ) {
         		int number = 8 * r + c; 
         		Button btn = new Button (String.valueOf((number)));
+        		btn.setShape( new Circle(1));
+        		btn.setPrefSize(90,90);
         		boardPane.add(btn,c , r);
         	}
         }
@@ -37,6 +46,8 @@ public class Main extends Application {
         ScrollPane scrollPane = new ScrollPane (boardPane) ; 
         
         stage.setScene(new Scene(scrollPane));
+        stage.setHeight(850);
+        stage.setWidth(825);
         stage.show();
     }
 
