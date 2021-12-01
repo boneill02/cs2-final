74a0e6d46d2f66d4a6b2e105f5e5af01efa3c53b
Ben O'Neill <ben@benoneill.xyz>
2021-11-30 19:22:32 -0500

    remove module-info, add to gitignore

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.cs2final {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires javafx.graphics;
-
-
-    opens edu.wit.comp1050.cs2final to javafx.fxml;
-    exports edu.wit.comp1050.cs2final;
-}
