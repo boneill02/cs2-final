56761a990063f61b34cfa3701e3c1c0026308034
Ben O'Neill <ben@benoneill.xyz>
2021-11-30 20:53:55 -0500

    add module-info.java back

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.cs2final {
+    requires javafx.controls;
+    requires javafx.fxml;
+	requires javafx.graphics;
+
+
+    opens edu.wit.comp1050.cs2final to javafx.fxml;
+    exports edu.wit.comp1050.cs2final;
+} 
