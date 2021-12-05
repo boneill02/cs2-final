9f3280c50697a86f3f5320f35f9ba59a64d2fe62
Ben O'Neill <ben@benoneill.xyz>
2021-12-05 16:19:09 -0500

    move buttons to Game class, display pieces based on owner

--- src/main/java/edu/wit/comp1050/cs2final/Game.java
+++ src/main/java/edu/wit/comp1050/cs2final/Game.java
@@ -3,14 +3,21 @@ package edu.wit.comp1050.cs2final;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
 
 /**
  * Checkers game mechanics.
  */
 public class Game {
     Board board;
-    Player player1, player2;
+    Player p1, p2;
+    Button[][] boardButtons;
 
+    public Game() {
+        p1 = new HumanPlayer(Direction.UP);
+        p2 = new HumanPlayer(Direction.DOWN);
+        Board b = new Board(p1, p2);
+    }
 
     /**
      * Initialize the game.
@@ -20,8 +27,40 @@ public class Game {
      */
     public Game(Board board, Player player1, Player player2) {
         this.board = board;
-        this.player1 = player1;
-        this.player2 = player2;
+        this.p1 = player1;
+        this.p2 = player2;
+    }
+
+    public void displayPieces(Board b) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Piece p = b.getPiece(i, j);
+                if (p == null)
+                    continue;
+
+                if (p.getOwner() == p1 && p.isKing()) {
+                    boardButtons[i][j].setText("WK");
+                } else if (p.getOwner() == p1 && !p.isKing()) {
+                    boardButtons[i][j].setText("W");
+                } else if (p.getOwner() == p2 && p.isKing()) {
+                    boardButtons[i][j].setText("BK");
+                } else if (p.getOwner() == p2 && !p.isKing()) {
+                    boardButtons[i][j].setText("B");
+                }
+            }
+        }
+    }
+
+    public void initButtons(GridPane pane) {
+        boardButtons = new Button[8][8];
+        for ( int r = 0; r < 8; r++) {
+            for ( int c = 0; c < 8; c++) {
+                int number = 8 * c + r;
+                boardButtons[r][c] = new Button (String.valueOf((number)));
+                boardButtons[r][c].setShape( new Rectangle(90, 90));
+                pane.add(boardButtons[r][c], r, c);
+            }
+        }
     }
 
     /**
@@ -30,10 +69,13 @@ public class Game {
     public void start() {
         boolean running = true;
 
+        /* TODO implement Player.doTurn() */
+        /*
         while (running) {
-            player1.doTurn();
-            player2.doTurn();
+            p1.doTurn();
+            p2.doTurn();
         }
+         */
     }
 
 }
--- src/main/java/edu/wit/comp1050/cs2final/Main.java
+++ src/main/java/edu/wit/comp1050/cs2final/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 import javafx.stage.Stage;
@@ -20,6 +21,8 @@ import java.io.IOException;
  * 
  */
 public class Main extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         /* set window properties */
@@ -33,17 +36,8 @@ public class Main extends Application {
         boardPane.setPadding(new Insets(10));
         boardPane.setHgap(10);
         boardPane.setVgap(10);
-        
-        for ( int r = 0; r < 8;r ++) { 
-        	for ( int c = 0; c < 8; c ++ ) {
-        		int number = 8 * r + c; 
-        		Button btn = new Button (String.valueOf((number)));
-        		btn.setShape( new Circle(1));
-        		btn.setPrefSize(90,90);
-        		boardPane.add(btn,c , r);
-        	}
-        }
-        
+
+
         ScrollPane scrollPane = new ScrollPane (boardPane) ; 
         //Setting the correct layout 
         stage.setScene(new Scene(scrollPane));
@@ -52,6 +46,9 @@ public class Main extends Application {
         stage.show();
         // TODO rest of UI
 
+        Game g = new Game();
+        g.initButtons(boardPane);
+        g.start();
     }
 
     public static void main(String[] args) {
